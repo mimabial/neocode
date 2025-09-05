1ade08dd03d5c8737a9db9c6df24c5521ad88398
mimabial <mabialamirion@hotmail.fr>
2025-09-05 21:15:22 +0200

    fix deprecation nordic

--- lua/plugins/themes/colorscheme.lua
+++ lua/plugins/themes/colorscheme.lua
@@ -187,7 +187,7 @@ return {
           variants = {},
           apply_variant = function() return false end,
           set_transparency = function(enable)
-            pcall(require("nordic").setup, { transparent_bg = enable })
+            pcall(require("nordic").setup, { transparent = enable })
             return true
           end,
         },
@@ -987,7 +987,7 @@ return {
       require("nordic").setup({
         bold_keywords = false,
         italic_comments = true,
-        transparent_bg = false,
+        transparent = false,
         bright_border = false,
         reduced_blue = true,
         swap_backgrounds = false,
